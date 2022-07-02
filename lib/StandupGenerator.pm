a115645570eb014b0a99a3febb77b2df2821f3db
jtreeves <jackson.t.reeves@gmail.com>
2022-07-02 15:38:39 -0400

    Stub out remaining subroutines

--- lib/StandupGenerator.pm
+++ lib/StandupGenerator.pm
@@ -1,19 +1,5 @@
 package StandupGenerator;
 
-# Execute this subroutine from the shell via this command:
-# perl -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::open_standup("/Users/jtreeves/Dropbox/Programming/mock-standups", 2, 7)'
-# Replace current sting inside innermost double-quotes with path for whatever directory will contain the standups, then follow with sprint number and day number
-
-sub open_standup {
-    my ($path, $sprint, $day) = @_;
-    my $cmd = "open ${path}/s${sprint}d${day}.txt";
-    system($cmd);
-}
-
-sub create_standup {
-
-}
-
 sub find_last_file {
     my ($path) = @_;
     opendir my $dir, $path;
@@ -22,7 +8,6 @@ sub find_last_file {
     my @sorted = sort compare_files @files;
     my $files_length = scalar(@files);
     my $last_file = $sorted[$files_length - 1];
-    print($last_file);
     return $last_file;
 }
 
@@ -49,6 +34,30 @@ sub compare_files {
             return 0;
         }
     }
+}
+
+sub create_standup {
+    my ($path) = @_;
+    my $last = find_last_file $path;
+    print($last);
+}
+
+# Execute this subroutine from the shell via this command:
+# perl -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::open_standup("/Users/jtreeves/Dropbox/Programming/mock-standups", 2, 7)'
+# Replace current sting inside innermost double-quotes with path for whatever directory will contain the standups, then follow with sprint number and day number
+
+sub open_standup {
+    my ($path, $sprint, $day) = @_;
+    my $cmd = "open ${path}/s${sprint}d${day}.txt";
+    system($cmd);
+}
+
+sub view_standups_from_week {
+
+}
+
+sub set_aliases {
+    # csu, osu, wsu
 }
 
 1;
