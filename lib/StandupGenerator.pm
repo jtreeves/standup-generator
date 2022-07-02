e754eb4c68462e2b799dacbee454d164dd1b3549
jtreeves <jackson.t.reeves@gmail.com>
2022-07-02 14:00:07 -0400

    [open_standup] Adjust open_file to take additional params for sprint and day

--- lib/StandupGenerator.pm
+++ lib/StandupGenerator.pm
@@ -1,12 +1,12 @@
 package StandupGenerator;
 
 # Execute this command from the shell via this command:
-# perl -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::open_file("~/.viminfo")'
-# Replace current sting inside innermost double-quotes with path for whatever file you wish to open
+# perl -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::open_standup("/Users/jtreeves/Dropbox/Programming/mock-standups", 2, 7)'
+# Replace current sting inside innermost double-quotes with path for whatever directory will contain the standups, then follow with sprint number and day number
 
-sub open_file {
-    my ($path) = @_;
-    my $cmd = "open ${path}";
+sub open_standup {
+    my ($path, $sprint, $day) = @_;
+    my $cmd = "open ${path}/s${sprint}d${day}.txt";
     system($cmd);
 }
 
