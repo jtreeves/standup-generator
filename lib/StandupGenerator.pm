e19c570814ed50131c0267432e1e9b02ae2e4562
jtreeves <jackson.t.reeves@gmail.com>
2022-07-03 22:24:07 -0400

    [StandupGenerator] Add comments to the methods

--- lib/StandupGenerator.pm
+++ lib/StandupGenerator.pm
@@ -12,6 +12,7 @@ our @EXPORT = qw(
     set_aliases
 );
 
+# Creates a new standup file when given a directory, with identifiers that increment off of the directory's last file and contents pulled from yesterday's standup file 
 sub create_standup {
     my ($path) = @_;
     StandupGenerator::Creator::create_new($path);
@@ -21,11 +22,13 @@ sub create_standup {
 # perl -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::open_standup("/Users/jtreeves/Dropbox/Programming/mock-standups", 2, 7)'
 # Replace current sting inside innermost double-quotes with path for whatever directory will contain the standups, then follow with sprint number and day number
 
+# Opens an existing standup file when given a directory, a sprint number, and a two-digit string for the day
 sub open_standup {
     my ($path, $sprint, $day) = @_;
     StandupGenerator::Accessor::open_one($path, $sprint, $day);
 }
 
+# Opens all standup files in a directory from the past week, along with the coming Monday's standup file, if they exist
 sub view_standups_from_week {
     my ($path) = @_;
     StandupGenerator::Accessor::open_many($path);
@@ -35,6 +38,7 @@ sub view_standups_from_week {
 # perl -Ilib -e 'require "./lib/StandupGenerator.pm"; StandupGenerator::set_aliases("/Users/jtreeves/Dropbox/Programming/mock-standups")'
 # Replace innermost string with path to whatever directory you want to use
 
+# Append some functions to the user's configuration file to make it easier for the user to execute key methods from this package via the CLI, with all functions automatically referencing the correct directory
 sub set_aliases {
     my ($path) = @_;
     StandupGenerator::Manipulator::save_script_shortcuts($path);
