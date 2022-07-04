4fd6208d169695e075f607957ec417551458e8e2
jtreeves <jackson.t.reeves@gmail.com>
2022-07-03 21:14:53 -0400

    [Creator] Add comments to method

--- lib/StandupGenerator/Creator.pm
+++ lib/StandupGenerator/Creator.pm
@@ -8,6 +8,7 @@ our @EXPORT = qw(
     create_new
 );
 
+# Create a new standup file containing contents from the previous day's standup, then open the file for further editing
 sub create_new {
     my ($path) = @_;
     my $last_file = StandupGenerator::Helper::find_last_file($path);
@@ -19,9 +20,11 @@ sub create_new {
     my $next_file_day;
 
     if ($last_file_day == '0') {
+        # Yesterday's standup was for day 10 of the last sprint, so today's standup will be for day 1 of the new sprint
         $next_file_day = '01';
         $next_file_sprint = $last_file_sprint + 1;
     } else {
+        # Continue incrementing days within the current sprint
         $next_day = $last_file_day + 1;
         $next_file_day = "0${next_day}";
         $next_file_sprint = $last_file_sprint;
@@ -34,29 +37,35 @@ sub create_new {
     my $next_file = "s${next_file_sprint}d${next_file_day}.txt";
     my $next_file_path = "${path}/${next_file}";
 
+    # Store key data from yesterday's standup
     open my $fh, '<', $last_file_path;
     my $last_file_content = do { local $/; <$fh> };
     close($fh);
+    my $file_length = length($last_file_content);
     my $today_index = index($last_file_content, 'TODAY') + 6;
     my $blockers_index = index($last_file_content, 'BLOCKERS') + 9;
-    my $file_length = length($last_file_content);
     my $today_content;
     my $blockers_content;
 
     if ($last_file eq "s0d0.txt") {
+        # When creating first standup within a directory, give sections empty bullets
         $today_content = "- ";
         $blockers_content = "- ";
     } else {
+        # Store yesterday's TODAY and BLOCKERS sections in order to repurpose them for today's standup
         $today_content = substr($last_file_content, $today_index, $blockers_index - $today_index - 11);
         $blockers_content = substr($last_file_content, $blockers_index, $file_length - $blockers_index);
     }
 
+    # Connect gathered pieces to form a single string with all necessary contents
     my $next_file_content = "STANDUP: SPRINT ${next_file_sprint} - DAY ${next_file_day}\n\nYESTERDAY\n${today_content}\n\nTODAY\n${today_content}\n\nBLOCKERS\n${blockers_content}";
 
+    # Create new file
     open my $new_fh, '>', $next_file_path;
     print $new_fh $next_file_content;
     close($new_fh);
 
+    # Open new file
     StandupGenerator::Accessor::open_one($path, $next_file_sprint, $next_file_day);
 
     return $next_file;
